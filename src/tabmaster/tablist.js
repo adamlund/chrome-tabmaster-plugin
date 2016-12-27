b483908c18898d7098100d5ecab5622537069474
Adam Lund <adam.lund@thomsonreuters.com>
2016-12-27 09:08:28 -0600

    Minor ui tweaks, removed dist as chrome no longer supports

--- src/tabmaster/tablist.js
+++ src/tabmaster/tablist.js
@@ -17,10 +17,8 @@ function getTabs(tabs) {
 var tab_count = 0;
 
 function start(){
-  console.log("tab search loaded");
   chrome.windows.getAll({ "populate" : true }, listWindows);
 }
-
 
 function popWindow(tab){
   chrome.windows.update(tab.windowId, { "focused" : true }, function(){ });
@@ -31,28 +29,34 @@ function popTab(tab){
 }
 
 function listTabs(tabs){
-  var j = 0;
-  for( j; j < tabs.length; j++){
+  var j = tabs.length-1,
+    windiv = document.createElement("DIV");
+
+  windiv.setAttribute("class", "windowbox");
+
+  for( j; j >= 0; j--){
     var tab = tabs[j];
-    addTabTitle(tab.title, tab.id, tab.windowId, tab.url, tab.favIconUrl);
+    windiv.appendChild( addTabTitle(tab.title, tab.id, tab.windowId, tab.url, tab.favIconUrl) );
   }
+
+  document.getElementById("open_tabs").appendChild(windiv);
+
   tab_count +=tabs.length;
   updateTabCount(tab_count);
 }
 
 function listWindows(windows){
   var numWindows = windows.length;
-  var i = 0;
+  var i = windows.length-1;
 
   document.getElementById("window-count").innerHTML = ""+numWindows;
 
-  console.log(" "+ numWindows + " open windows");
-
-  for( i; i < numWindows; i++){
+  for( i; i >= 0; i--){
     var thewin = windows[i];
     chrome.tabs.getAllInWindow(thewin.id, listTabs);
   }
 }
+
 function iconError(source){
   source.src = "img/icon-blank-tab.svg";
   return true;
@@ -63,42 +67,26 @@ function updateTabCount(count){
 }
 
 function addTabTitle(title, id, windowid, url, favIconUrl){
-  var namediv = document.createElement("DIV");
+  var tabref    = document.createElement("BUTTON"),
+    favicon     = document.createElement("IMG"),
+    titletxt    = document.createTextNode(title),
+    row         = document.createElement("DIV");
 
-  var tabidattr = document.createAttribute("data-tabid");
-  tabidattr.value = id;
-  namediv.setAttributeNode(tabidattr);
+  tabref.setAttribute("data-tabid", id);
+  tabref.setAttribute("data-windowid", windowid);
+  tabref.setAttribute("title", url);
 
-  var winidattr = document.createAttribute("data-windowid");
-  winidattr.value = windowid;
-  namediv.setAttributeNode(winidattr);
+  favicon.setAttribute("src", (typeof favIconUrl === 'undefined' ? "img/icon-blank-tab.svg" : favIconUrl));
+  tabref.appendChild(favicon);
+  tabref.appendChild(titletxt);
 
-  var titleurl = document.createAttribute("title");
-  titleurl.value = url;
-  namediv.setAttributeNode(titleurl);
+  row.appendChild(tabref);
 
-  var favicon = document.createElement("IMG");
-  var faviconsrc = document.createAttribute("src");
-  faviconsrc.value = typeof favIconUrl === 'undefined' ? "img/icon-blank-tab.svg" : favIconUrl;
-  favicon.setAttributeNode(faviconsrc);
-
-  namediv.appendChild(favicon);
-
-  var titletxt = document.createTextNode(title);
-  namediv.appendChild(titletxt);
-
-
-
-
-  document.getElementById("open_tabs").appendChild(namediv);
-  namediv.addEventListener('click', function() {
-    var myid = this.getAttribute("data-tabid");
-    var mywinid = this.getAttribute("data-windowid");
-    console.log("tab id is:"+myid+" win id:"+mywinid);
-
-    chrome.tabs.get(parseInt(myid), popTab);
-
+  tabref.addEventListener('click', function() {
+    chrome.tabs.get(parseInt(this.getAttribute("data-tabid")), popTab);
   });
+
+  return row;
 }
 
 function renderStatus(statusText) {
@@ -107,8 +95,6 @@ function renderStatus(statusText) {
 
 //chrome.browserAction.onClicked.addListener(start);
 
-
-document.addEventListener('DOMContentLoaded', function() { 
+document.addEventListener('DOMContentLoaded', function() {
   start();
 });
-
