875769987099f8bccbfeca6eb07861083967631f
Adam Lund <adam.lund@thomsonreuters.com>
2017-04-21 09:42:13 -0500

    Redesigned and refactored

---
+++ src/tabmaster/background.js
@@ -0,0 +1,7 @@
+/**
+ * Created by AdamLund on 4/19/17.
+ */
+
+chrome.commands.onCommand.addEventListener(function(cmd){
+  alert("Command hit",cmd);
+});
--- src/tabmaster/tablist.js
+++ src/tabmaster/tablist.js
@@ -1,100 +1,189 @@
-// Copyright (c) 2016 Adam Lund
+// Copyright (c) 2017 Adam Lund
+
+var TabMasterModel = {
+
+  init: function () {
+    var self = this;
+
+    this.myWindowIDs = [];
+    this.myTabs = [];
+
+    chrome.windows.getAll({"populate": true}, function getWindows(windows) {
+
+      var done = false;
+
+      windows.forEach(function (win, windex) {
+        self.myWindowIDs.push(win.id);
+
+        // each window, get its tabs
+        chrome.tabs.getAllInWindow(win.id, function getTabs(tabs) {
+
+          tabs.forEach(function (tab, tabindex) {
+
+            self.myTabs.push(tab);
+
+            // check for the last tab of the last window -- this is the final callback
+            if(windex === (windows.length-1) && tabindex === (tabs.length-1)) {
+              done = true;
+            }
+
+          });
+
+          // once all callbacks are complete set the DOM
+          if(done) {
+            self.renderList();
+          }
+
+        });
+
+      });
+
+    });
+
+  },
+  showTab: function(tab){
+
+    chrome.tabs.update(tab.id, { "active" : true, "highlighted" : true }, function(tab){
+
+      chrome.windows.update(tab.windowId, { "focused" : true }, function(){ });
+
+    });
+  },
+
+  renderList: function(){
+    var self = this;
+    var container = document.createElement("DIV");
+
+    document.getElementById("tab-count").innerHTML = ""+self.myTabs.length;
+    document.getElementById("window-count").innerHTML = ""+self.myWindowIDs.length;
 
 
-/*
-function getWindows(win) {
-  targetWindow = win;
-  chrome.tabs.getAllInWindow(targetWindow.id, getTabs);
-}
 
-function getTabs(tabs) {
-  tabCount = tabs.length;
-  // We require all the tab information to be populated.
-  chrome.windows.getAll({"populate" : true}, moveTabs);
-}
-*/
+    self.myTabs.forEach(function(tab){
+      var row = self.renderTab(tab);
+      container.appendChild(row);
+    });
 
-var tab_count = 0;
+    document.getElementById("open_tabs").appendChild(container);
 
-function start(){
-  chrome.windows.getAll({ "populate" : true }, listWindows);
-}
+    this.clearButton(false);
+  },
 
-function popWindow(tab){
-  chrome.windows.update(tab.windowId, { "focused" : true }, function(){ });
-}
+  renderTab: function(tab){
+    var tabref    = document.createElement("BUTTON"),
+     favicon      = document.createElement("IMG"),
+     titletxt     = document.createElement("SPAN"),
+     row          = document.createElement("DIV");
 
-function popTab(tab){
-  chrome.tabs.update(tab.id, { "active" : true, "highlighted" : true }, popWindow)
-}
+    tabref.setAttribute("data-tabid", tab.id);
+    tabref.setAttribute("data-windowid", tab.windowId);
+    tabref.setAttribute("data-url", tab.url);
 
-function listTabs(tabs){
-  var j = tabs.length-1,
-    windiv = document.createElement("DIV");
+    favicon.setAttribute("src", (typeof tab.favIconUrl === 'undefined' ? "img/icon-blank-tab.svg" : tab.favIconUrl));
 
-  windiv.setAttribute("class", "windowbox");
+    tabref.appendChild(favicon);
+    titletxt.innerText = tab.title;
+    tabref.appendChild(titletxt);
 
-  for( j; j >= 0; j--){
-    var tab = tabs[j];
-    windiv.appendChild( addTabTitle(tab.title, tab.id, tab.windowId, tab.url, tab.favIconUrl) );
+    row.appendChild(tabref);
+
+    tabref.addEventListener('click', function() {
+      chrome.tabs.get( parseInt(this.getAttribute("data-tabid")), TabMasterModel.showTab );
+    });
+
+    favicon.addEventListener('error', function(err){
+      err.target.src = "img/icon-blank-tab.svg";
+      err.preventDefault();
+    });
+
+    return row;
+  },
+
+  filterTabs: function(search_term){
+
+    var self = this;
+
+    if(!search_term || search_term.length === 0){
+      self.clearList();
+      self.renderList();
+      return false;
+    }
+
+    var results = this.myTabs.filter(function(the_tab){
+      return the_tab.title.includes(search_term) || the_tab.url.includes(search_term);
+    });
+
+    self.clearList();
+
+    if(!results || results.length === 0){
+      var note = document.createElement("div");
+      note.className = "notif";
+      note.setAttribute('role', 'alert');
+      note.textContent = "No results matching "+search_term;
+      document.getElementById("open_tabs").appendChild(note);
+      self.clearButton(true);
+      return false;
+    }
+
+    results.forEach(function(tab){
+      document.getElementById("open_tabs").appendChild(self.renderTab(tab));
+    });
+    self.clearButton(true);
+
+  },
+
+  clearList: function () {
+    var node = document.getElementById("open_tabs");
+    var last;
+    while (last = node.lastChild) node.removeChild(last);
+    this.mySearchField.focus();
+  },
+
+  attachListeners: function(search_elem, button_elem){
+    var self = this;
+    this.mySearchField = search_elem;
+    this.myClearButton = button_elem;
+
+    search_elem.addEventListener('keyup', function(event) {
+      self.filterTabs( event.target.value );
+    });
+    button_elem.addEventListener('click', function(event) {
+      search_elem.value = "";
+      self.clearList();
+      self.renderList();
+    });
+  },
+
+  clearButton: function(show_or_no){
+    if(this.myClearButton){
+      switch(show_or_no){
+        case true:
+          if(this.myClearButton.classList.contains("show")){
+            return false;
+          }
+          this.myClearButton.className = "show";
+          break
+        case false:
+          if(this.myClearButton.classList.contains("hide")){
+            return false;
+          }
+          this.myClearButton.className = "hide";
+          break;
+      }
+      return true;
+    }
+  },
+  searchHistory: function(searchterm){
+    chrome.history.search(function(history_array){
+
+    });
   }
-
-  document.getElementById("open_tabs").appendChild(windiv);
-
-  tab_count +=tabs.length;
-  updateTabCount(tab_count);
-}
-
-function listWindows(windows){
-  var numWindows = windows.length;
-  var i = windows.length-1;
-
-  document.getElementById("window-count").innerHTML = ""+numWindows;
-
-  for( i; i >= 0; i--){
-    var thewin = windows[i];
-    chrome.tabs.getAllInWindow(thewin.id, listTabs);
-  }
-}
-
-function iconError(source){
-  source.src = "img/icon-blank-tab.svg";
-  return true;
-}
-
-function updateTabCount(count){
-  document.getElementById("tab-count").innerHTML = ""+count;
-}
-
-function addTabTitle(title, id, windowid, url, favIconUrl){
-  var tabref    = document.createElement("BUTTON"),
-    favicon     = document.createElement("IMG"),
-    titletxt    = document.createTextNode(title),
-    row         = document.createElement("DIV");
-
-  tabref.setAttribute("data-tabid", id);
-  tabref.setAttribute("data-windowid", windowid);
-  tabref.setAttribute("title", url);
-
-  favicon.setAttribute("src", (typeof favIconUrl === 'undefined' ? "img/icon-blank-tab.svg" : favIconUrl));
-  tabref.appendChild(favicon);
-  tabref.appendChild(titletxt);
-
-  row.appendChild(tabref);
-
-  tabref.addEventListener('click', function() {
-    chrome.tabs.get(parseInt(this.getAttribute("data-tabid")), popTab);
-  });
-
-  return row;
-}
-
-function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
-}
-
-//chrome.browserAction.onClicked.addListener(start);
+};
 
 document.addEventListener('DOMContentLoaded', function() {
-  start();
+
+  var tabModel = Object.create(TabMasterModel);
+  tabModel.init();
+  tabModel.attachListeners(document.getElementById('tabSearch'), document.getElementById('resetButton'));
+
 });
