ca4fb4fb16fa448824c46e27af47d6af1a9e1c60
Adam Lund <adam.lund@thomsonreuters.com>
2017-05-05 09:53:20 -0500

    Preparing submittal to chrome web store

--- src/tabmaster/background.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by AdamLund on 4/19/17.
- */
-
-chrome.commands.onCommand.addEventListener(function(cmd){
-  alert("Command hit",cmd);
-});
--- src/tabstacks/tabstacks.js
+++ src/tabstacks/tabstacks.js
@@ -1,40 +1,36 @@
-// Copyright (c) 2017 Adam Lund
+/** Copyright (c) 2017 Adam Lund http://adamlund.design **/
+
+/**
+ * TabMasterModel JS
+ * Uses a basic form of MVC for presenting opened tabs to the user.
+ * tabstacks.js functions as the model-view, Chromium as the actual controller, and the popup html as the view
+ *
+ * No external JS dependencies.
+ * No calls to external services via http at any time.
+ * All JavaScript other than provided by the Chromium and Browser API, are written by the author.
+ *
+ */
 
 var TabMasterModel = {
-
   init: function () {
+
     var self = this;
 
     this.myWindowIDs = [];
     this.myTabs = [];
+    this.MSPERDAY = 86400000; //milliseconds per day
+    this.SEARCH_DURATION = 5; //days
 
-    chrome.windows.getAll({"populate": true}, function getWindows(windows) {
+    chrome.tabs.query({}, function getAllTheTabs(tabs){
 
-      var done = false;
+      tabs.forEach(function (tab, tabindex) {
 
-      windows.forEach(function (win, windex) {
-        self.myWindowIDs.push(win.id);
+        self.myTabs.push(tab);
 
-        // each window, get its tabs
-        chrome.tabs.getAllInWindow(win.id, function getTabs(tabs) {
-
-          tabs.forEach(function (tab, tabindex) {
-
-            self.myTabs.push(tab);
-
-            // check for the last tab of the last window -- this is the final callback
-            if(windex === (windows.length-1) && tabindex === (tabs.length-1)) {
-              done = true;
-            }
-
-          });
-
-          // once all callbacks are complete set the DOM
-          if(done) {
-            self.renderList();
-          }
-
-        });
+        // check for the last tab of the last window -- this is the final callback
+        if(tabindex === (tabs.length-1)) {
+          self.renderList();
+        }
 
       });
 
@@ -51,17 +47,27 @@ var TabMasterModel = {
   },
 
   renderList: function(){
-    var self = this;
-    var list = document.createElement("ul");
+    var self = this,
+     list = document.createElement("ul"),
+     i = self.myTabs.length;
+
+    self.myWindowIDs = [];
+
+    while(i--){
+
+      if(!self.myWindowIDs.includes(self.myTabs[i].windowId))
+        self.myWindowIDs.push(self.myTabs[i].windowId);
+
+      list.appendChild(self.___renderTab( self.myTabs[i] ) );
+
+    }
 
     document.getElementById("tab-count").innerHTML = ""+self.myTabs.length;
     document.getElementById("window-count").innerHTML = ""+self.myWindowIDs.length;
 
-    self.myTabs.forEach(function(tab){
-      list.appendChild(self.___renderTab(tab));
-    });
-
     document.getElementById("open_tabs").appendChild(list);
+
+    this.___setNote(""+self.myTabs.length+" open tabs", null, false);
 
     this.clearButton(false);
   },
@@ -87,11 +93,11 @@ var TabMasterModel = {
 
     row.appendChild(tabref);
 
-    tabref.addEventListener('click', function() {
+    tabref.addEventListener('click', function __clickAndShowTab() {
       chrome.tabs.get( parseInt(this.getAttribute("data-tabid")), TabMasterModel.showTab );
     });
 
-    favicon.addEventListener('error', function(err){
+    favicon.addEventListener('error', function __faviconLoadError(err){
       err.target.src = "img/icon-blank-tab.svg";
       err.preventDefault();
     });
@@ -99,12 +105,36 @@ var TabMasterModel = {
     return row;
   },
 
+  ___setNote: function(textLine1, textLine2, visible){
+
+    this.clearList("note");
+
+    var note = document.createElement("div");
+    note.className = visible ? "notif" : "accessibilityText";
+    note.setAttribute('role', 'alert');
+    note.textContent = textLine1;
+
+    document.getElementById("note").appendChild(note);
+
+    // search recent browser history, user might have closed the tab
+    if(textLine2){
+      var sh = document.createElement("div");
+      sh.textContent = textLine2;
+      note.appendChild(sh);
+      return sh;
+    }
+
+    return note;
+
+  },
+
   filterTabs: function(search_term){
 
     var self = this;
 
     if(!search_term || search_term.length === 0){
-      self.clearList();
+      self.clearList("open_tabs");
+      self.clearList("note");
       self.renderList();
       return false;
     }
@@ -114,24 +144,17 @@ var TabMasterModel = {
 
     // do check for
     var results = this.myTabs.filter(function(the_tab){
-      return the_tab.url.includes(search_term) || re.test(the_tab.title);
+      return re.test(the_tab.url) || re.test(the_tab.title);
     });
 
-    self.clearList();
+    self.clearList("open_tabs");
+    self.clearList("note");
 
     if(!results || results.length === 0){
-      var note = document.createElement("div");
-      note.className = "notif";
-      note.setAttribute('role', 'alert');
-      note.textContent = "No open tabs matching "+search_term;
-      document.getElementById("open_tabs").appendChild(note);
+
+      var sh = self.___setNote("No open tabs matching \""+search_term+"\"", "searching history...", true);
+
       self.clearButton(true);
-
-      // search recent (24hrs) browser history, user might have closed the tab
-      var sh = document.createElement("div");
-      sh.textContent = "searching history...";
-      note.appendChild(sh);
-
       self.searchHistory(search_term, sh);
       return false;
     }
@@ -143,11 +166,12 @@ var TabMasterModel = {
     });
     document.getElementById("open_tabs").appendChild(list);
     self.clearButton(true);
+    self.___setNote(""+results.length+" tabs matching \""+search_term+"\"", null, true);
 
   },
 
-  clearList: function () {
-    var node = document.getElementById("open_tabs");
+  clearList: function (element_id) {
+    var node = document.getElementById(element_id);
     var last;
     while (last = node.lastChild) node.removeChild(last);
     this.mySearchField.focus();
@@ -158,12 +182,13 @@ var TabMasterModel = {
     this.mySearchField = search_elem;
     this.myClearButton = button_elem;
 
-    search_elem.addEventListener('keyup', function(event) {
+    search_elem.addEventListener('keyup', function __keypressAndFilterEvent(event) {
       self.filterTabs( event.target.value );
     });
-    button_elem.addEventListener('click', function(event) {
+    button_elem.addEventListener('click', function __clearAndRenderEvent(event) {
       search_elem.value = "";
-      self.clearList();
+      self.clearList("open_tabs");
+      self.clearList("note");
       self.renderList();
     });
   },
@@ -176,7 +201,7 @@ var TabMasterModel = {
             return false;
           }
           this.myClearButton.className = "show";
-          break
+          break;
         case false:
           if(this.myClearButton.classList.contains("hide")){
             return false;
@@ -190,6 +215,7 @@ var TabMasterModel = {
   __makelink: function(history_item){ // pass a history item
     var link = document.createElement("A");
     link.setAttribute("href", history_item.url);
+    link.setAttribute("title", history_item.url);
     link.textContent = history_item.title && history_item.title.length > 0 ? history_item.title : history_item.url;
     link.setAttribute("target", "_blank");
     return link;
@@ -198,26 +224,41 @@ var TabMasterModel = {
     var self = this;
     var list = document.createElement("ul");
 
+    history_array.sort(function(a, b){
+      return b.visitCount - a.visitCount;
+    });
+
     history_array.forEach(function(histitem){
-      var li = document.createElement("LI");
+      var li = document.createElement("LI"),
+       note = document.createElement("DIV");
+
       li.appendChild(self.__makelink(histitem));
+
+      note.innerHTML = "Last visited: "+new Date(histitem.lastVisitTime).toDateString();
+      note.innerHTML += histitem.visitCount > 1 ? " viewed <strong>"+histitem.visitCount +"</strong> times" : "";
+
+      li.appendChild(note);
+      li.className = "histItem";
       list.appendChild(li);
+
     });
     return list;
   },
   searchHistory: function(searchterm, textUpdate){
 
-    var self = this;
+    var self = this, newtime;
+    newtime = Date.now() - (self.SEARCH_DURATION  * self.MSPERDAY ); // search within # days of search duration
 
-    chrome.history.search({ text: searchterm, maxResults: 100}, function(history_array){
+    chrome.history.search({ text: searchterm, startTime: newtime, maxResults: 25}, function(history_array){
 
       if(history_array && history_array.length > 0){
-        textUpdate.textContent = " "+history_array.length+" links found in history";
-
+        self.clearList("open_tabs");
+        textUpdate.textContent = " "+history_array.length;
+        textUpdate.textContent += history_array.length > 1 ? " links in history" : " link in history";
         document.getElementById("open_tabs").appendChild(self.__renderLinks(history_array));
       }
       else {
-        textUpdate.textContent = "";
+        textUpdate.textContent = "No history items found either. Searched back to "+new Date(newtime).toDateString();
       }
 
     });
